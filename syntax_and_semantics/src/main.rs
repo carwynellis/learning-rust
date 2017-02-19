aa93441e5394c3fb809a8437b3b157e83ab36268
Carwyn Ellis <carwynellis@gmail.com>
2017-02-19 16:46:08 +0000

    Added Primitive Types examples

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -3,6 +3,7 @@
 fn main() {
     variable_bindings();
     functions();
+    primitive_types();
 }
 
 fn variable_bindings() {
@@ -111,4 +112,65 @@ fn add_one(x: i32) -> i32 {
 // A return keyword is available should early or explicit returns be required.
 fn add_two(x: i32) -> i32 {
     return x + 2;
+}
+
+fn primitive_types() {
+    println!("Primitive Types\n");
+
+    // Rust has primitive boolean types
+    let x = true;
+    let y: bool = false;
+
+    println!("x is {}, y is {}", x, y);
+
+    // Char represents a single unicode character
+    // Note - char is thus 4 bytes
+    let some_char: char = 'c';
+
+    println!("some_char is {}", some_char);
+
+    // Rust has a range of signed and unsigned numeric types.
+    // Where no explicit type is provided rust defaults as follows
+    let some_int = 42;    // defaults to i32
+    let some_float = 1.0; // defaults to f64
+
+    println!("some_int is {}, some_float is {}", some_int, some_float);
+
+    // Arrays
+    let nums = [ 1, 2, 3 ];
+
+    println!("nums contains {} elements", nums.len());
+
+    // Rust also provides shorthand for array initialization
+    let nums2 = [ 0 ; 20 ];
+
+    println!("nums2 contains {} elements", nums2.len());
+
+    // Array elements are accessed using zero-based subscripts
+    let names = [ "Foo", "Bar", "Baz" ];
+
+    println!("third name is {}", names[2]);
+
+    // In rust a slice offers a 'view' into a data structure without
+    // dupliating the contents of that structure.
+    let a = [ 0, 1, 2, 3, 4 ];
+
+    let slice_all = &a[..]; // refers to all elements of a
+    let slice_mid = &a[1..4]; // refers to elements 1, 2, 3
+
+    println!("slice_all has {} elements", slice_all.len());
+    println!("slice_mid has {} elements", slice_mid.len());
+
+    // Rust also offers tuples
+    let some_tuple = (1, "hello");
+
+    // Elements of a tuple can be accessed using a destructuring let.
+    let (t1, t2) = some_tuple;
+    println!("some_tuple contains ( {}, {} )", t1, t2);
+
+    // Tuple fields can also be accessed with indexing syntax.
+    // Note - like arrays, the indexes are zero based
+    println!("second value of some_tuple is {}", some_tuple.1);
+
+    println!();
 }
