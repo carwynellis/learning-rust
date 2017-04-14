fae0129a961d471c67959577236837c0378630d1
Carwyn Ellis <carwynellis@gmail.com>
2017-04-14 18:32:08 +0100

    Added lifetime example
    * added simple example showing how lifetimes can be declared in cases
      where the compiler needs annotations to resolve lifetime ambiguity

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     loops();
     vectors();
     ownership();
-    borrowing();
+    references_and_borrowing();
+    lifetimes();
 }
 
 fn variable_bindings() {
@@ -387,8 +388,8 @@ fn double_i32(i: i32) -> i32 {
     i * 2
 }
 
-fn borrowing() {
-    println!("Borrowing\n");
+fn references_and_borrowing() {
+    println!("References and Borrowing\n");
 
     // Immutable references can be borrowed as follows.
     let v1 = vec![1, 2, 3];
@@ -410,6 +411,8 @@ fn borrowing() {
     }
 
     println!("x is now {}", x);
+
+    println!();
 }
 
 // Folds over a vec to compute the sum.
@@ -419,4 +422,55 @@ fn sum_vec(v: &Vec<i32>) -> i32 {
 
 fn sum_two_vecs(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
     sum_vec(v1) + sum_vec(v2)
+}
+
+fn lifetimes() {
+    println!("Lifetimes\n");
+
+    // When passing references we can run into ambiguity about whether the
+    // referenced resource will still exist at the point that we attempt to use
+    // it.
+
+    // Rust provides lifetime annotations which allow the lifetime of
+    // references to be explicitly annotated. The compiler can then make use of
+    // these annotations to determine whether the code is safe to run or not.
+
+    // Lifetime annotations are usually required for more complex scenarios
+    // since the compiler is able to check simpler cases without assistance.
+
+    // Example using function that must declare lifetimes.
+    let mut data = vec![1, 2, 3, 4, 5, 6, 7];
+
+    let filtered;
+
+    // We introduce a new scope to create ambiguity about the lifetime of one
+    // of the parameters we will pass to a function.
+    // This will not compile if the less_than function does not provide
+    // lifetime annotations.
+    // In this case filtered is in an outer scope relative to limit, which is
+    // in an inner scope.
+    // The function will need to declare two scopes and associate the result
+    // with the scope where it will be used, in this case, the outer scope.
+    {
+        let limit = 5;
+        filtered = less_than(&mut data, &limit);
+    }
+
+    println!(
+        "Filtered numbers: {}",
+        filtered
+            .iter()
+            .fold(String::new(), |acc, &elem| acc + &elem.to_string() + ", ")
+    );
+
+    println!();
+}
+
+// This function must explicitly declare lifetimes for the incoming parameter
+// references, and the result.
+// In this case we declare that the lifetime of the result will share the outer
+// scope of the incoming data parameter.
+fn less_than<'outer, 'inner>(data: &'outer mut Vec<i32>, limit: &'inner i32) -> &'outer Vec<i32> {
+    data.retain(|&d| d < *limit);
+    return data;
 }
