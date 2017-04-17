e9679c7fe3fe3d57ddc078d682a03d11eaf804c0
Carwyn Ellis <carwynellis@gmail.com>
2017-04-17 19:04:08 +0100

    Added Enum examples

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -16,6 +16,7 @@ fn main() {
     lifetimes();
     mutability();
     structs();
+    enums();
 }
 
 fn variable_bindings() {
@@ -626,6 +627,36 @@ fn structs() {
     // no members. This can be handy in specific situations, for example when
     // extending a trait that requires a Struct to be declared that is not
     // needed in the implementation.
+
+    println!();
+}
+
+// TODO - unused enum entries generate compiler warnings in this example.
+fn enums() {
+    println!("Enums\n");
+
+    // Enums in Rust are quite familiar. However one difference is that each
+    // entry in the enum may contain additional optional values specific to
+    // that entry.
+    enum Message {
+        Quit,
+        ChangeColor(i32, i32, i32),
+        Move { x: i32, y: i32 },
+        Write(String),
+    }
+
+    // Note that the syntax for each entry is similar to that for structs.
+    // Values may optionally be given names as in the Move entry above.
+
+    // A given enum entry is accessed using :: as follows.
+
+    let message = Message::Move { x: 1, y: 2 };
+
+    // Note that the entry names are scoped to the enum type, so different
+    // enums can share entry names.
+
+    // Enum values cannot be destructured directly, but values can be extracted
+    // using pattern matching.
 
     println!();
 }
