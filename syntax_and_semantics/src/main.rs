aaf5b9209dad0dfd34643957f667b2e9c05d1267
Carwyn Ellis <carwynellis@gmail.com>
2017-09-17 18:20:09 +0100

    Added universal function call syntax examples

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -33,6 +33,7 @@ fn main() {
     if_let_while_let();
     trait_objects();
     closures();
+    universal_function_call_syntax();
 }
 
 fn variable_bindings() {
@@ -1535,6 +1536,57 @@ fn closures() {
     // Further information about the underlying implementation and how to
     // return a closure is available at
     // https://doc.rust-lang.org/book/first-edition/closures.html
+
+    println!();
+}
+
+fn universal_function_call_syntax() {
+    println!("Universal Function Call Syntax\n");
+
+    // Sometimes instances of functions can have the same name, for example
+    // functions defined in traits as follows.
+    trait Foo {
+            fn f(&self);
+    }
+
+    trait Bar {
+            fn f(&self);
+    }
+
+    struct Baz;
+
+    impl Foo for Baz {
+            fn f(&self) { println!("Baz’s impl of Foo"); }
+    }
+
+    impl Bar for Baz {
+            fn f(&self) { println!("Baz’s impl of Bar"); }
+    }
+
+    let b = Baz;
+
+    // Baz has access to two implementations of f, so if we try to call b.f()
+    // the compiler would error out with multiple applicable methods in scope.
+
+    // b.f(); // triggers a compiler error
+
+    // In this instance we can work around this by using universal function
+    // call syntax to disambiguate the implementations as follows.
+
+    Foo::f(&b);
+    Bar::f(&b);
+
+    println!();
+
+    // The invocations above are using a short hand form. A more verbose angle
+    // bracket syntax is also available, and is needed in certain situations.
+    //
+    // <Type as Trait>::method(args);
+    //
+    // The type provides a type hint for the compiler.
+
+    <Baz as Foo>::f(&b);
+    <Baz as Bar>::f(&b);
 
     println!();
 }
