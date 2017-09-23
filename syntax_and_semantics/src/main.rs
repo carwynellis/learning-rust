182d227eae6caf7a87a6602a48b08243d059e923
Carwyn Ellis <carwynellis@gmail.com>
2017-09-23 19:24:27 +0100

    Cleaned up module usage with pub use declarations

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -1,29 +1,31 @@
 extern crate syntax_and_semantics;
 
+use syntax_and_semantics::examples::*;
+
 // Examples from Rust Book - Syntax and Semantics.
 // See https://doc.rust-lang.org/book/first-edition/syntax-and-semantics.html
 fn main() {
-    syntax_and_semantics::examples::variable_bindings::variable_bindings();
-    syntax_and_semantics::examples::functions::functions();
-    syntax_and_semantics::examples::primitive_types::primitive_types();
-    syntax_and_semantics::examples::if_examples::if_examples();
-    syntax_and_semantics::examples::loops::loops();
-    syntax_and_semantics::examples::vectors::vectors();
-    syntax_and_semantics::examples::ownership::ownership();
-    syntax_and_semantics::examples::references_and_borrowing::references_and_borrowing();
-    syntax_and_semantics::examples::lifetimes::lifetimes();
-    syntax_and_semantics::examples::mutability::mutability();
-    syntax_and_semantics::examples::structs::structs();
-    syntax_and_semantics::examples::enums::enums();
-    syntax_and_semantics::examples::matching::matching();
-    syntax_and_semantics::examples::patterns::patterns();
-    syntax_and_semantics::examples::method_syntax::method_syntax();
-    syntax_and_semantics::examples::strings::strings();
-    syntax_and_semantics::examples::generics::generics();
-    syntax_and_semantics::examples::traits::traits();
-    syntax_and_semantics::examples::drop::drop();
-    syntax_and_semantics::examples::if_let_while_let::if_let_while_let();
-    syntax_and_semantics::examples::trait_objects::trait_objects();
-    syntax_and_semantics::examples::closures::closures();
-    syntax_and_semantics::examples::universal_function_call_syntax::universal_function_call_syntax();
+    variable_bindings();
+    functions();
+    primitive_types();
+    if_examples();
+    loops();
+    vectors();
+    ownership();
+    references_and_borrowing();
+    lifetimes();
+    mutability();
+    structs();
+    enums();
+    matching();
+    patterns();
+    method_syntax();
+    strings();
+    generics();
+    traits();
+    drop();
+    if_let_while_let();
+    trait_objects();
+    closures();
+    universal_function_call_syntax();
 }
